edf69649f40e107955f9e171c62373520d5f0a88
Aneesh Panda <aneeshpanda.ap@gmail.com>
2021-06-18 14:03:48 +0530

    Footer Added to oganiser login and sing up

--- Organiser Login and SignUp/nav.js
+++ Organiser Login and SignUp/nav.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
     $("#nav").load("../Header/header.txt");
-    // $("#foobottom").load("../Footer/footer.txt");
+    $("#foobottom").load("../Footer/footer.txt");
 });

0aeb22f84e6cd3a575391c7ae5abe667d1952cb1
Aneesh Panda <aneeshpanda.ap@gmail.com>
2021-07-03 00:48:18 +0530

    User Flow

--- JS/teamwohack.js
+++ JS/teamwohack.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
-    $("#nav").load("../Header/headerl.txt");
-    $("#foobottom").load("../Footer/footer.txt");
+    $("#nav").load("../Assets/Header/headerl.txt");
+    $("#foobottom").load("../Assets/Footer/footer.txt");
 });

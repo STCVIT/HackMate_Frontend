bd83e8471858291072b7a6cd43c17d656bd59a03
Aneesh Panda <aneeshpanda.ap@gmail.com>
2021-07-11 15:17:16 +0530

    View Hackathon Buttons

--- JS/viewhackathon.js
+++ JS/viewhackathon.js
@@ -10,48 +10,71 @@ const popular = document.querySelector(".popular");
 
 all.addEventListener("click", function () {
   for (var i = 0; i < all.classList.length; i++) {
-    if (all.classList[i] === "button") {
+    if (all.classList[i] == "button") {
       all.classList.remove("button");
       all.classList.add("button1");
-    }
-    else if (all.classList[i] === "button1") {
+    } else if (all.classList[i] == "button1") {
       all.classList.remove("button1");
       all.classList.add("button");
+      ongoing.classList.remove("button");
+      ongoing.classList.add("button1");
+      upcoming.classList.remove("button");
+      upcoming.classList.add("button1");
+      popular.classList.remove("button");
+      popular.classList.add("button1");
     }
   }
 });
 ongoing.addEventListener("click", function () {
   for (var i = 0; i < ongoing.classList.length; i++) {
-    if (ongoing.classList[i] === "button") {
+    if (ongoing.classList[i] == "button") {
       ongoing.classList.remove("button");
       ongoing.classList.add("button1");
-    }
-    else if (ongoing.classList[i] === "button1") {
+    } else if (ongoing.classList[i] == "button1") {
       ongoing.classList.remove("button1");
       ongoing.classList.add("button");
+      all.classList.remove("button");
+      all.classList.add("button1");
+      upcoming.classList.remove("button");
+      upcoming.classList.add("button1");
+      popular.classList.remove("button");
+      popular.classList.add("button1");
+
     }
   }
 });
 upcoming.addEventListener("click", function () {
   for (var i = 0; i < upcoming.classList.length; i++) {
-    if (upcoming.classList[i] === "button") {
+    if (upcoming.classList[i] == "button") {
       upcoming.classList.remove("button");
       upcoming.classList.add("button1");
-    } else if (upcoming.classList[i] === "button1") {
+    } else if (upcoming.classList[i] == "button1") {
       upcoming.classList.remove("button1");
       upcoming.classList.add("button");
+      all.classList.remove("button");
+      all.classList.add("button1");
+      ongoing.classList.remove("button");
+      ongoing.classList.add("button1");
+      popular.classList.remove("button");
+      popular.classList.add("button1");
     }
   }
 });
 
 popular.addEventListener("click", function () {
   for (var i = 0; i < popular.classList.length; i++) {
-    if (popular.classList[i] === "button") {
+    if (popular.classList[i] == "button") {
       popular.classList.remove("button");
       popular.classList.add("button1");
-    } else if (popular.classList[i] === "button1") {
+    } else if (popular.classList[i] == "button1") {
       popular.classList.remove("button1");
       popular.classList.add("button");
+      all.classList.remove("button");
+      all.classList.add("button1");
+      ongoing.classList.remove("button");
+      ongoing.classList.add("button1");
+      upcoming.classList.remove("button");
+      upcoming.classList.add("button1");
     }
   }
 });

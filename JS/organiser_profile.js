e835f2b44bf582e5966fdcea2f7abc3898cd7d98
Megha Maitin <meghamaitin27@gmail.com>
2021-07-12 22:58:51 +0530

    js commented

--- JS/organiser_profile.js
+++ JS/organiser_profile.js
@@ -2,22 +2,26 @@ $(document).ready(function () {
     $("#nav").load("../Assets/Header/headerwobtn.txt");
     $("#foobottom").load("../Assets/Footer/footer.txt");
 });
-const form=document.getElementById("form");
-form.addEventListener('submit',(e)=>{
-    (e).preventDefault();
-    const name=document.getElementById('name').value;
-    const phone_num=document.getElementById('phone_num').value;
-    const college=document.getElementById('college').value;
-    const website=document.getElementById('website').value;
-    checkInputs(name,phone_num,college);
+// const form=document.getElementById("form");
+// form.addEventListener('submit',(e)=>{
+//     (e).preventDefault();
+//     const name=document.getElementById('name').value;
+//     const phone_num=document.getElementById('phone_num').value;
+//     const college=document.getElementById('college').value;
+//     const website=document.getElementById('website').value;
+//     checkInputs(name,phone_num,college);
 
-    form.reset()
-});
+//     form.reset()
+// });
+// function checkInputs(name,phone_num,college){
+//     name=name.toUpperCase();
+//     let n =name.length;
+//     if(n<=30){
+//         let value=/^[A-Z][A-Z\s]*$/
+//         if(!name.match(value)){
+//             alert("invalid name");
+//         }
+//     }
+//     let num=/^\d{10}$/;
 
-function checkInputs(name,phone_num,college){
-    name=name.toUpperCase();
-    let n =name.length;
-    if(n<=30){
-        if()
-    }
-}
+// }

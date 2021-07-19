cd109dbe2016824118fd449894f6800043aac6f1
Megha Maitin <meghamaitin27@gmail.com>
2021-07-19 14:28:10 +0530

    teams.js

--- JS/teams.js
+++ JS/teams.js
@@ -2,3 +2,35 @@ $(document).ready(function () {
     $("#nav").load("../Assets/Header/headerl.txt");
     $("#foobottom").load("../Assets/Footer/footer.txt");
 });
+
+axios(`${url}/DN_Team/myTeams`, {
+    headers: {
+      Authorization: "Bearer " + auth,
+    },
+  })
+    .then((response) => {
+      teams = response.data;
+      console.log(teams);
+      for (var i = 0; i < teams.length; i++) {
+        document.querySelector(".cards").innerHTML +=
+        "<div class='card1' style='max-width: 497px; max-height: 371px;'><div class='card-body'><h5 class='card-title'>"+
+        teams[i].name +
+        "</h5><p><text>Hackathon:</text><hackathon>"
+        teams[i].hackathon+
+        "</hackathon></p>div class='card-details'><p><f>"
+        teams[i].numberofmembers+
+        "</f><r> Team <br> Members</r></p><div class='vl'></div><ul class='team-members'>"
+        for (var j=0; j<numberofmembers; j++) {
+            "<li class='list-item'><img id='pp' src='../Assets/Images/Rectangle 155.svg'><p>"
+            teams[i].member1+
+            "<br><t>"
+            teams[i].member1domain
+            "</t></p>"
+        }
+        "</ul></div></div></div>"
+    }     
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+

afea63f42e41d3438d4ebed12459d2c76ac57542
Aneesh Panda <aneeshpanda.ap@gmail.com>
2021-08-17 15:42:19 +0530

    Code Resturctured + Firebase Configs Added + Image Upload for Edit Hack

--- JS/orghack.js
+++ JS/orghack.js
@@ -5,36 +5,6 @@ $(document).ready(function () {
   $("#foobottom").load("../Assets/Footer/footer.txt");
 });
 
-// axios(`${url}/organiser/hacks?page=1`, {
-//   headers: {
-//     Authorization: "Bearer " + auth,
-//   },
-// })
-//   .then((response) => {
-//     hacks = response.data;
-//     console.log(hacks);
-
-//     for (var i = 0; i < hacks.newHacks.length; i++) {
-//       document.querySelector(".wrapper").innerHTML +=
-//         "<div class='box'> <a href='./orghackprofile.html' style='text-decoration: none;'> <div class='innertxt'> <div class='hackname'> <nb1>" +
-//         hacks.newHacks[i].name +
-//         ". </nb1> </div> <br> <div class='dates'> <div class='box1 start'> <div class='nbg'> <nbg>Begins:</nbg> <div class='nbw'> <nbw>" +
-//         hacks.newHacks[i].start.split("T")[0] +
-//         "</nbw> </div> </div> </div> <div class='box2 end'> <div class='nbg'> <nbg>Ends:</nbg> <div class='nbw'> <nbw>" +
-//         hacks.newHacks[i].end.split("T")[0] +
-//         "</nbw> </div> </div> </div> </div> <div class='nbg'> <nbg>Venue: </nbg> <nbw>" +
-//         hacks.newHacks[i].venue +
-//         "</nbw> </div> <div class='nbg'> <nbg>Max Team Size: </nbg> <nbw> " +
-//         hacks.newHacks[i].max_team_size +
-//         " Participants</nbw> </div> <div class='nbg'> <nbg>Prize Pool: </nbg> <nbw></nbw>" +
-//         hacks.newHacks[i].prize_pool +
-//         "</div> <div class='status'> <circle class='circle1'></circle> <circle class='circle2'></circle> <circle class='circle3'></circle>" +
-//         hacks.newHacks[i].mode_of_conduct +
-//         "</div> </div> </a></div>";
-//     }
-//   })
-//   .catch((error) => console.error("Error: " + error));
-
 function events(event) {
   console.log(event);
   page = event.target.innerHTML;

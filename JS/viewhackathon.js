beebb1e6a74ebe55b8223078b654536a75327f2d
Aneesh Panda <aneeshpanda.ap@gmail.com>
2021-08-17 19:14:42 +0530

    Changes made to config

--- JS/viewhackathon.js
+++ JS/viewhackathon.js
@@ -137,7 +137,7 @@ function getdeets(event) {
   occurence = event.target.id;
 
   console.log(occurence);
-  
+
   displayTeams();
 }
 
@@ -303,59 +303,66 @@ function nextPage() {
     page = Pagination.page + 1;
   }
   console.log(page);
-  axios(
-    `${url}/getHacks/${occurence}?page=${page}`,
-    {
-      headers: {
-        Authorization: "Bearer " + auth,
-      },
-    }
-  )
-    .then((response) => {
-      hacks = response.data;
-      console.log(hacks);
+  let authid;
+  firebase.auth().currentUser.getIdToken().then((id)=>{
+    console.log("Hey!")
+    console.log(id)
+    authid = id;
 
-      document.querySelector(".wrapper").innerHTML = "";
-      for (var i = 0; i < hacks.final.length; i++) {
-        document.querySelector(".wrapper").innerHTML +=
-          "<div class='box'><img src='../Assets/Images/Name Banner.png' class='namebanner' alt=''> <div class='innertxt'> <nb1 class='hackname'>" +
-          hacks.final[i].name +
-          " <a target='_blank' href='https://" +
-          hacks.final[i].website +
-          "'><img src='../Assets/Images/Hack Link.svg' alt=''></a></nb1> <br> <div class='dates'> <div class='box1 start'> <div class='nbg'> <nbg>Begins:</nbg> <div class='nbw'> <nbw class='startd'>" +
-          //hacks.final[i].start.split("T")[0] +
-          new Date(hacks.final[i].start.split("T")[0]).toString().split(" ")[2] +
-          " " +
-          new Date(hacks.final[i].start.split("T")[0]).toString().split(" ")[1] +
-          " " +
-          new Date(hacks.final[i].start.split("T")[0]).toString().split(" ")[3] +
-          " " +
-          "</nbw> </div> </div> </div> <div class='box2 end'> <div class='nbg'> <nbg>Ends:</nbg> <div class='nbw'> <nbw class='endd'>" +
-          //hacks.final[i].end.split("T")[0] +
-          new Date(hacks.final[i].end.split("T")[0]).toString().split(" ")[2] +
-          " " +
-          new Date(hacks.final[i].end.split("T")[0]).toString().split(" ")[1] +
-          " " +
-          new Date(hacks.final[i].end.split("T")[0]).toString().split(" ")[3] +
-          " " +
-          "</nbw> </div> </div> </div> </div> <div class='nbg'> <nbg>Venue: </nbg> <nbw class='venue'>" +
-          hacks.final[i].venue +
-          "</nbw> </div> <div class='nbg'> <nbg>Team Size: </nbg> <nbw class='ts'>" +
-          +hacks.final[i].min_team_size +
-          "-" +
-          hacks.final[i].max_team_size +
-          "</nbw><nbw> Participants</nbw> </div> <div class='nbg'> <nbg>Prize Pool: </nbg> <nbw></nbw>" +
-          hacks.final[i].prize_pool +
-          "</div><img class='mode_of_conduct pt-3' src='../Assets/Images/" +
-          hacks.final[i].mode_of_conduct + ".svg'>" +
-          "<a class='btnkm btn btn-success' href='./hackdetails.html?" +
-          hacks.final[i]._id +
-          "' role='button'>Know More</a> </div> </div>";
+    axios(
+      `${url}/getHacks/${occurence}?page=${page}`,
+      {
+        headers: {
+          Authorization: "Bearer " + authid,
+        },
       }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+    )
+      .then((response) => {
+        hacks = response.data;
+        console.log(hacks);
+  
+        document.querySelector(".wrapper").innerHTML = "";
+        for (var i = 0; i < hacks.final.length; i++) {
+          document.querySelector(".wrapper").innerHTML +=
+            "<div class='box'><img src='../Assets/Images/Name Banner.png' class='namebanner' alt=''> <div class='innertxt'> <nb1 class='hackname'>" +
+            hacks.final[i].name +
+            " <a target='_blank' href='https://" +
+            hacks.final[i].website +
+            "'><img src='../Assets/Images/Hack Link.svg' alt=''></a></nb1> <br> <div class='dates'> <div class='box1 start'> <div class='nbg'> <nbg>Begins:</nbg> <div class='nbw'> <nbw class='startd'>" +
+            //hacks.final[i].start.split("T")[0] +
+            new Date(hacks.final[i].start.split("T")[0]).toString().split(" ")[2] +
+            " " +
+            new Date(hacks.final[i].start.split("T")[0]).toString().split(" ")[1] +
+            " " +
+            new Date(hacks.final[i].start.split("T")[0]).toString().split(" ")[3] +
+            " " +
+            "</nbw> </div> </div> </div> <div class='box2 end'> <div class='nbg'> <nbg>Ends:</nbg> <div class='nbw'> <nbw class='endd'>" +
+            //hacks.final[i].end.split("T")[0] +
+            new Date(hacks.final[i].end.split("T")[0]).toString().split(" ")[2] +
+            " " +
+            new Date(hacks.final[i].end.split("T")[0]).toString().split(" ")[1] +
+            " " +
+            new Date(hacks.final[i].end.split("T")[0]).toString().split(" ")[3] +
+            " " +
+            "</nbw> </div> </div> </div> </div> <div class='nbg'> <nbg>Venue: </nbg> <nbw class='venue'>" +
+            hacks.final[i].venue +
+            "</nbw> </div> <div class='nbg'> <nbg>Team Size: </nbg> <nbw class='ts'>" +
+            +hacks.final[i].min_team_size +
+            "-" +
+            hacks.final[i].max_team_size +
+            "</nbw><nbw> Participants</nbw> </div> <div class='nbg'> <nbg>Prize Pool: </nbg> <nbw></nbw>" +
+            hacks.final[i].prize_pool +
+            "</div><img class='mode_of_conduct pt-3' src='../Assets/Images/" +
+            hacks.final[i].mode_of_conduct + ".svg'>" +
+            "<a class='btnkm btn btn-success' href='./hackdetails.html?" +
+            hacks.final[i]._id +
+            "' role='button'>Know More</a> </div> </div>";
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+})
 }
 function prevPage() {
   if (page > 1) {
